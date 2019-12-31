86368e1eaeca62942c278f6ccbff51cf026a8ed8
Zsolt Molnár <mozb17@gmail.com>
2019-12-31 14:46:03 +0100

    Add restaurants tab

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,30 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> Restaurants;
+        public InMemoryRestaurantData()
+        {
+            Restaurants = new List<Restaurant>()
+            {
+                new Restaurant() { Id = 0, Name = "PizzaCastle", Location = "Italy", Cuisine = CuisineType.Italian },
+                new Restaurant() { Id = 1, Name = "Gulasch", Location = "Hungary", Cuisine = CuisineType.Hungarian },
+                new Restaurant() { Id = 2, Name = "Mexico", Location = "Mexico", Cuisine = CuisineType.Mexican }
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return Restaurants;
+        }
+    }
+}
